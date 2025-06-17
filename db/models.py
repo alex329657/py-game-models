3717cd83ffc8d17ee29f08a7050e2ecc96c8f5f8
Alexandr K. <alex329657@gmail.com>
2025-06-17 11:40:33 +0300

    'Solution2'

--- db/models.py
+++ db/models.py
@@ -14,8 +14,7 @@ class Skill(models.Model):
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race,
                              on_delete=models.CASCADE,
-                             related_name="skills"
-                             )
+                             related_name="skills")
 
     def __str__(self) -> str:
         return self.name
@@ -23,7 +22,7 @@ class Skill(models.Model):
 
 class Guild(models.Model):
     name = models.CharField(unique=True, max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
@@ -35,14 +34,12 @@ class Player(models.Model):
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race,
                              on_delete=models.CASCADE,
-                             related_name="players"
-                             )
+                             related_name="players")
     guild = models.ForeignKey(Guild,
                               on_delete=models.SET_NULL,
                               null=True,
                               blank=True,
-                              related_name="members"
-                              )
+                              related_name="members")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
--- main.py
+++ main.py
@@ -17,10 +17,8 @@ def main() -> None:
         for skill_data in race_data.get("skills", []):
             Skill.objects.get_or_create(
                 name=skill_data["name"],
-                defaults={
-                    "bonus": skill_data["bonus"],
-                    "race": race
-                }
+                race=race,
+                defaults={"bonus": skill_data["bonus"]}
             )
 
         guild_data = player_data.get("guild")
